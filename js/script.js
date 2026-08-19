9f7f8c1bb258ec611443098581c8995f11415900
Alfez1645 <alfez1605@gmail.com>
2026-08-19 12:43:06 +0530

    make signature dish section

--- js/script.js
+++ js/script.js
@@ -21,6 +21,20 @@ tl.to(".hero-box" , {
     duration:0.6,
 })
 
+// 
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to("#sign-dish-box", {
+    y: 0,
+    opacity: 1,
+    duration: 1,
+    stagger: 0.2,
+    scrollTrigger: {
+        trigger: "#sign-dish-box",
+        start: "top 80%"
+    }
+});
+// 
 // menu button
 const navlink = document.querySelector(".nav-link");
 const menubtn = document.querySelector("#Navbar i");
